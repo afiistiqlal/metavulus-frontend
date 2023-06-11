eef7b9c59271fe9c6075e08b3030a7cc887f4c3e
afiistiqlal <afiistiqlal@gmail.com>
2023-06-11 13:51:32 +0700

    [Update] Maintenance page background wave color

--- components/templates/MaintenancePage.tsx
+++ components/templates/MaintenancePage.tsx
@@ -3,6 +3,7 @@ import Layout from "./Layout";
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "/public/Metavulus Logo_PNG_Icon Color.png";
+import Wave from "/public/images/maintenanceWave.png";
 import HomePageSubHeader from "../molecules/HomePageSubHeader";
 import Subtitle from "../atoms/Subtitle";
 import Paragraph from "../atoms/Paragraph";
@@ -13,34 +14,41 @@ type Props = {};
 const MaintenancePage = (props: Props) => {
   return (
     <Layout>
-      <div className="py-8 px-10 flex flex-col gap-4 w-2/4">
-        <Link href={"#"}>
-          <Image
-            className="w-[50px] mb-6"
-            src={Logo}
-            alt=""
-            width={100}
-            height={10}
-          />
-        </Link>
-        <div className="flex">
-          <HomePageSubHeader text="Ooooops" />{" "}
-          <span className="text-mv-primary-1">
-            <HomePageSubHeader text="." />
-          </span>
-        </div>
-        <Subtitle text="We are currently under maintenance" />
-        <Paragraph text="We're currently performing scheduled maintenance to improve our services. We apologize for the inconvenience caused." />
-        <div className="flex justify-between border rounded-3xl p-px focus:border-4">
-          <input
-            type="text"
-            className="px-6 text-mv-gray-3 focus:outline-current flex-auto rounded-3xl"
-            placeholder="@ email address"
-          />
-          <div className="">
-            <ButtonPrimary text="Notify Me" />
+      <div className="flex min-w-full min-h-screen max-h-fit">
+        <div className="py-8 px-10 flex flex-col gap-4 w-2/4 z-10">
+          <Link href={"#"}>
+            <Image
+              className="w-[50px] mb-6"
+              src={Logo}
+              alt=""
+              width={100}
+              height={10}
+            />
+          </Link>
+          <div className="flex">
+            <HomePageSubHeader text="Ooooops" />{" "}
+            <span className="text-mv-primary-1">
+              <HomePageSubHeader text="." />
+            </span>
+          </div>
+          <Subtitle text="We are currently under maintenance" />
+          <Paragraph text="We're currently performing scheduled maintenance to improve our services. We apologize for the inconvenience caused." />
+          <div className="flex justify-between border rounded-3xl p-px focus:border-4">
+            <input
+              type="text"
+              className="px-6 text-mv-gray-3 focus:outline-current flex-auto rounded-3xl"
+              placeholder="@ email address"
+            />
+            <div className="">
+              <ButtonPrimary text="Notify Me" />
+            </div>
           </div>
         </div>
+        <Image
+          src={Wave}
+          alt="wave"
+          className="absolute top-o overflow-hidden max-w-full max-h-full"
+        />
       </div>
     </Layout>
   );

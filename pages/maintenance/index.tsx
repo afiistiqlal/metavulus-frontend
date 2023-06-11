8cb773843358ad1132f648de2d10a4bbccd802c3
afiistiqlal <afiistiqlal@gmail.com>
2023-06-11 12:53:14 +0700

    [New] Maintenance page

---
+++ pages/maintenance/index.tsx
@@ -0,0 +1,10 @@
+import MaintenancePage from "@/components/templates/MaintenancePage";
+import React from "react";
+
+type Props = {};
+
+const Maintenance = (props: Props) => {
+  return <MaintenancePage />;
+};
+
+export default Maintenance;
